dcc2a944a180e8b53c49ece36eb4e27c1c42bff9
Bernice2001 <wangechibanim@gmail.com>
2020-08-29 19:14:23 +0300

    created a form for quotes

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote: Quote;
+  @Output() isComplete:EventEmitter <boolean> = new EventEmitter();
+  @Output() upVote:EventEmitter <Quote> = new EventEmitter();
+  @Output() downVote:EventEmitter <Quote> = new EventEmitter();
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  
+  // numberofdislikes : number=0
+
+ likebuttonclick(quote){
+ this.upVote.emit(quote);
+ 
+}
+  dislikebuttonclick(quote){
+  this.downVote.emit(quote)
+}
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
